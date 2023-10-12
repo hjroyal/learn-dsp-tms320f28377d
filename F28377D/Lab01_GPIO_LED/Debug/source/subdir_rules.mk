a48f5339459b805fa0fb49d1075a18e2327a34b7
Baby Cat <hjroyal@foxmail.com>
2023-10-12 16:13:04 +0800

    点亮LED

--- F28377D/Lab01_GPIO_LED/Debug/source/subdir_rules.mk
+++ F28377D/Lab01_GPIO_LED/Debug/source/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 source/%.obj: ../source/%.asm $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"D:/ti/CCS9.2/ccs/tools/compiler/ti-cgt-c2000_20.2.3.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 --include_path="D:/ti/CCS9.2/28377YT/Lab01_GPIO_LED" --include_path="D:/ti/CCS9.2/28377YT/Lab01_GPIO_LED/include" --include_path="D:/ti/CCS9.2/ccs/tools/compiler/ti-cgt-c2000_20.2.3.LTS/include" --define=CPU1 -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
+	"D:/Coding/environment/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_20.2.2.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 --include_path="D:/Coding/AI/learn-dsp-tms320f28377d/F28377D/Lab01_GPIO_LED" --include_path="D:/Coding/AI/learn-dsp-tms320f28377d/F28377D/Lab01_GPIO_LED/include" --include_path="D:/Coding/environment/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_20.2.2.LTS/include" --define=CPU1 -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 source/%.obj: ../source/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: C2000 Compiler'
-	"D:/ti/CCS9.2/ccs/tools/compiler/ti-cgt-c2000_20.2.3.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 --include_path="D:/ti/CCS9.2/28377YT/Lab01_GPIO_LED" --include_path="D:/ti/CCS9.2/28377YT/Lab01_GPIO_LED/include" --include_path="D:/ti/CCS9.2/ccs/tools/compiler/ti-cgt-c2000_20.2.3.LTS/include" --define=CPU1 -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
+	"D:/Coding/environment/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_20.2.2.LTS/bin/cl2000" -v28 -ml -mt --cla_support=cla1 --float_support=fpu32 --tmu_support=tmu0 --vcu_support=vcu2 --include_path="D:/Coding/AI/learn-dsp-tms320f28377d/F28377D/Lab01_GPIO_LED" --include_path="D:/Coding/AI/learn-dsp-tms320f28377d/F28377D/Lab01_GPIO_LED/include" --include_path="D:/Coding/environment/ti/ccs920/ccs/tools/compiler/ti-cgt-c2000_20.2.2.LTS/include" --define=CPU1 -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="source/$(basename $(<F)).d_raw" --obj_directory="source" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
